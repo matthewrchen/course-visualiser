785650fc667c80e9291c4ad59ddf92706a9a66c6
Matthew Chen <264727661+matthewrchen@users.noreply.github.com>
2026-06-05 09:19:23 +1200

    updated computer systems page

--- src/app/specialisations/computer-systems-engineering/page.tsx
+++ src/app/specialisations/computer-systems-engineering/page.tsx
@@ -1,5 +1,15 @@
+import Graph from "@/components/courseMap";
+
 export default function Page() {
-    return (
-        <p>Computer systems engineering</p>
-    );
+  return (
+    <>
+      <h1 className="font-sans text-4xl">Computer Systems Engineering</h1>
+      <br></br>
+      <p className="font-sans">
+        This shows the courses available for study in the Computer Systems specialisation.
+      </p>
+      <br></br>
+      <Graph specName="Computer Systems"/>
+    </>
+  );
 }
