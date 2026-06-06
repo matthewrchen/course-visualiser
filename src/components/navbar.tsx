706f59fa5eb27cedcf4a543e3f62a7eb48fe3e47
Matthew Chen <264727661+matthewrchen@users.noreply.github.com>
2026-06-06 12:57:17 +1200

    added rest of spec links to navigation bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,7 +11,7 @@ export default function NavBar({ toggleNavBar } : Props) {
 
             <ul className="mt-[95px] space-y-[25px]">
                 <li>
-                    <Link href="" className="ml-[10px] font-sans font-extrabold text-2xl text-white drop-shadow-lg">Home</Link>
+                    <Link href="/" className="ml-[10px] font-sans font-extrabold text-2xl text-white drop-shadow-lg">Home</Link>
                 </li>
 
                 <li className="border-t-2 border-gray-800">
@@ -19,9 +19,45 @@ export default function NavBar({ toggleNavBar } : Props) {
                 </li>
 
                 <li>
-                    <Link href="" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Electrical and Electronic</Link>
+                    <Link href="/specialisations/biomedical-engineering" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Biomedical</Link>
                 </li>
                 
+                <li>
+                    <Link href="/specialisations/chemical-and-materials-engineering" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Chemical and Materials</Link>
+                </li>
+
+                <li>
+                    <Link href="/specialisations/civil-engineering" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Civil</Link>
+                </li>
+
+                <li>
+                    <Link href="/specialisations/computer-systems-engineering" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Computer Systems</Link>
+                </li>
+
+                <li>
+                    <Link href="/specialisations/electrical-and-electronic-engineering" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Electrical and Electronic</Link>
+                </li>
+
+                <li>
+                    <Link href="/specialisations/engineering-science" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Engineering Science</Link>
+                </li>
+
+                <li>
+                    <Link href="/specialisations/mechanical-engineering" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Mechanical</Link>
+                </li>
+
+                <li>
+                    <Link href="/specialisations/mechatronics-engineering" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Mechatronics</Link>
+                </li>
+
+                <li>
+                    <Link href="/specialisations/software-engineering" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Software</Link>
+                </li>
+
+                <li>
+                    <Link href="/specialisations/structural-engineering" className="ml-[10px] font-sans font-extrabold text-xl text-white drop-shadow-lg">Structural</Link>
+                </li>
+
             </ul>
         </nav>
     )
