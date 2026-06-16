1f0b8832d42361c5ab5195248462ad137fa35237
Matthew Chen <264727661+matthewrchen@users.noreply.github.com>
2026-06-16 13:35:36 +1200

    created start of specialisation card component

---
+++ src/components/specialisation-card.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+type Props = {
+    name: string,
+    link: string
+}
+
+export default function Card({ name, link } : Props) {
+    return (
+        <Link href={link} className="flex justify-center h-64 aspect-square bg-gray-200 text-center">
+            <h1 className="mt-auto p-4">{name}</h1>
+        </Link>
+    );
+}
