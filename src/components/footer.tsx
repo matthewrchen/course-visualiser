b53121668e6344aac328e6454b1f8c2c0ff056ae
Matthew Chen <264727661+matthewrchen@users.noreply.github.com>
2026-06-16 19:42:11 +1200

    added disclaimer text to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="h-[100px] w-full bg-purple-950 shadow-md">
-            
+        <footer className="h-[100px] w-full flex justify-center items-center bg-purple-950 shadow-md">
+            <p className="font-sans text-xs text-white">This website is not endorsed by, directly affiliated with, maintained, authorised, or sponsored by The University of Auckland.</p>
         </footer>
     );
 }
